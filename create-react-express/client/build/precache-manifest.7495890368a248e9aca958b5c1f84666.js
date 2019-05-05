67677aad148cb159a0630873efa9805a54a714ef
Andromeda <hannalauth@gmail.com>
2019-05-04 17:50:07 -0700

    gh pages added

---
+++ create-react-express/client/build/precache-manifest.7495890368a248e9aca958b5c1f84666.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7a2468d06c032549e8df",
+    "url": "/static/js/main.6524c5fa.chunk.js"
+  },
+  {
+    "revision": "fda3cd49448d2a8e3bd2",
+    "url": "/static/js/2.6064ab8d.chunk.js"
+  },
+  {
+    "revision": "7a2468d06c032549e8df",
+    "url": "/static/css/main.d3fda4a9.chunk.css"
+  },
+  {
+    "revision": "543da2c7aef64f6b8b170a95bcda8c0e",
+    "url": "/index.html"
+  }
+];
